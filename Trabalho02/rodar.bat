3781017f6ad0e46ec2e95f2de7b0bac67c151118
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-07-27 17:40:38 -0300

    update

--- Trabalho02/rodar.bat
+++ Trabalho02/rodar.bat
@@ -1,5 +1,5 @@
 flex Trabalho02.lex
-bison -d Trabalho02.y
+bison -d Trabalho02.y --warnings cex
 gcc Trabalho02.tab.c lex.yy.c Compiler.c -o Trabalho02 -lm
 type teste.txt | .\Trabalho02.exe
 java -jar C:\Users\bruni\Downloads\jasmin-2.4\jasmin.jar output.j
